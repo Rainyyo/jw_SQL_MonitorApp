ccb4f1ad58b41c8e6941ce5e22016db9e6089b07
Rainyyo <wh18862759172@163.com>
2024-03-27 18:30:28 +0800

    上机测试按钮

---
+++ Model/Global.cs
@@ -0,0 +1,83 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonitorApp.Properties;
+using MySql.Data.MySqlClient;
+using System.Windows.Controls;
+using NLog;
+
+namespace MonitorApp.Model
+{
+    public static class Global
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        public static  MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+        public static void Ini()
+        {
+            NlogConfig();
+        }
+
+        public static void NlogConfig()
+        {
+            var config = new NLog.Config.LoggingConfiguration();
+
+            // Targets where to log to: File and Console
+            //应用程序当前目录
+            //短日期【2022-01-06】
+
+            //长日期【2022-01-06 14:05:20.4023】
+            //记录等级【Trace，Debug，Info，Warn，Error，Fatal】
+            //调用Nlog时输入的内容
+            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "${basedir}/logs/${shortdate}.log", Layout = "${longdate}|${level:uppercase=true}|${message}" };
+            var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+
+            // Rules for mapping loggers to targets
+            // 该方法所设置节点的属性必须关联其它的兄弟节点或者属性为布尔值
+            config.AddRule(NLog.LogLevel.Info, NLog.LogLevel.Fatal, logconsole);
+            config.AddRule(NLog.LogLevel.Debug, NLog.LogLevel.Fatal, logfile);
+
+            // Apply config           
+            NLog.LogManager.Configuration = config;
+        }
+       
+        public static async void Insert_pc_data_alarm(string 报警代码, string 报警描述, string value)//设备报警描述
+        {
+            await Task.Delay(100);
+            Task data_alarm = Task.Run(() => {
+                var connection = new MySqlConnection(builder.ConnectionString);
+                try
+                {
+                    // 打开连接
+                    connection.Open();
+                    // 执行插入数据的 SQL 语句
+                    string sql = "INSERT INTO pc_data_alarm (ALARMCODE, ALARMDES, ALARMVALUE, CREATETIME) " +
+                                 "VALUES (@AlarmCode, @AlarmDes, @AlarmValue, @CreateTime)";
+                    MySqlCommand command = new MySqlCommand(sql, connection);
+                    // 添加参数
+                    command.Parameters.AddWithValue("@AlarmCode", 报警代码);
+                    command.Parameters.AddWithValue("@AlarmDes", 报警描述);
+                    command.Parameters.AddWithValue("@AlarmValue", value);//报警1， 复位0
+                    command.Parameters.AddWithValue("@CreateTime", DateTime.Now);
+
+                    command.ExecuteNonQuery();
+
+                    logger.Debug("设备报警描述数据添加成功！");
+                }
+                catch (Exception ex)
+                {
+                    logger.Error("设备报警描述异常" + ex);
+                }
+                finally
+                {
+                    // 关闭连接
+                    if (connection.State == System.Data.ConnectionState.Open)
+                        connection.Close();
+                }
+            });
+           
+        }
+    }
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -21,6 +21,7 @@ using MySql.Data.MySqlClient;
 using System.Globalization;
 using System.Collections.Generic;
 using System.Linq;
+using MonitorApp.Model;
 
 
 
@@ -83,6 +84,13 @@ namespace MonitorApp.ViewModels
             get { return fieldName; }
             set { SetProperty(ref fieldName, value); }
         }
+
+        private bool ButtonState;
+        public bool buttonState
+        {
+            get { return ButtonState; }
+            set { SetProperty(ref ButtonState, value); }
+        }
         #endregion
         #region 命令绑定
         private DelegateCommand appLoadedEventCommand;
@@ -97,6 +105,16 @@ namespace MonitorApp.ViewModels
         private DelegateCommand<string> testCommand;
         public DelegateCommand<string> TestCommand =>
             testCommand ?? (testCommand = new DelegateCommand<string>(ExecuteTestCommand));
+        private DelegateCommand inOutTime;
+        public DelegateCommand InOutTime =>
+            inOutTime ?? (inOutTime = new DelegateCommand(ExecuteInOutTime));
+
+        void ExecuteInOutTime()
+        {
+
+        }
+
+        
 
         void ExecuteTestCommand(string obj)
         {
@@ -104,67 +122,37 @@ namespace MonitorApp.ViewModels
             var connection = new MySqlConnection(builder.ConnectionString);
             switch (obj)
             {
-                case "查询数据库":
-                    try
-                    {
-                        //打开连接
-                        connection.Open();
-                        //定义命令
-                        cmd = "Show Databases;";
-                        var reader = new MySqlCommand(cmd, connection).ExecuteReader();
-                        while (reader.Read())
-                        {
-                            addMessage(reader.GetString(0));
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        addMessage("MySQL异常" + ex);
-                    }
-                    finally
-                    {
-                        // 关闭连接
-                        if (connection.State == System.Data.ConnectionState.Open)
-                            connection.Close();
-                        //addMessage("MySQL关闭连接");
-                    }
-                    break;
                 case "新建数据库":
-                    try
+
+                    connection.Open();
+
+                    string sql = "SELECT * FROM pc_data_alarm";
+
+                    using (MySqlCommand command = new MySqlCommand(sql, connection))
                     {
-                        // 打开连接
-                        connection.Open();
-                        // 检查数据库是否存在
-                        string databaseName = "JingWang_Database";
-                        if (!CheckIfDatabaseExists(connection, databaseName))
+                        using (MySqlDataReader reader = command.ExecuteReader())
                         {
-                            // 如果数据库不存在，则创建新的数据库
-                            CreateDatabase(connection, databaseName);
-                            addMessage($"数据库 {databaseName} 创建成功！");
+                            while (reader.Read())
+                            {
+                                int col1 = reader.GetInt32(0); // 第一列，假设是整数类型
+                                string col2 = reader.GetString(1); // 第二列，假设是字符串类型
+                                string col3 = reader.GetString(2); // 第三列，假设是字符串类型
+                                string col4 = reader.GetString(3); // 第四列，假设是字符串类型
+                                DateTime col5 = reader.GetDateTime(4); // 最后一列，
+                                addMessage($"\t{col1}\t{col2}\t{col3}\t{col4}\t{col5}");
+                            }
                         }
-                        else
-                        {
-                            addMessage($"数据库 {databaseName} 已经存在！");
-                        }
                     }
-                    catch (Exception ex)
-                    {
-                        addMessage("MySQL异常" + ex);
-                    }
-                    finally
-                    {
-                        // 关闭连接
-                        if (connection.State == System.Data.ConnectionState.Open)
-                            connection.Close();
-                    }
+
+
                     break;
                 case "写数据报警描述数据":
-                    ReadWaring(FileName);
+                    Global.Insert_pc_data_alarm("3", "3", "3");
                     break;
                 case "写工艺质量数据":
 
                     break;
-                    
+
 
             }
         }
@@ -211,7 +199,6 @@ namespace MonitorApp.ViewModels
             });
             Task.Run(() =>
             {
-                MySQL_ConnectionMsg();
                 MySqlConnection connection = new MySqlConnection(builder.ConnectionString);
                 bool r = false;
                 try
@@ -247,14 +234,14 @@ namespace MonitorApp.ViewModels
         public MainWindowViewModel(IContainerProvider containerProvider)
         {
             pLC = containerProvider.Resolve<IPLCService>("PLC");//初始化
-            NlogConfig();//日志文件
-            Task.Run(()=> { ReadWaring(FileName); });
-       }
+            MySQL_ConnectionMsg();
+            Global.Ini();
+        }
         #endregion
         #region 功能函数
         private void addMessage(string str)
         {
-            logger.Debug(str);
+            logger.Info(str);
             Debug.WriteLine(str);
             string[] s = MessageStr.Split('\n');
             if (s.Length > 1000)
@@ -267,64 +254,9 @@ namespace MonitorApp.ViewModels
             }
             MessageStr += DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + " " + str;
         }
-        private void NlogConfig()
-        {
-            var config = new NLog.Config.LoggingConfiguration();
-
-            // Targets where to log to: File and Console
-            //应用程序当前目录
-            //短日期【2022-01-06】
-
-            //长日期【2022-01-06 14:05:20.4023】
-            //记录等级【Trace，Debug，Info，Warn，Error，Fatal】
-            //调用Nlog时输入的内容
-            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "${basedir}/logs/${shortdate}.log", Layout = "${longdate}|${level:uppercase=true}|${message}" };
-            var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
-
-            // Rules for mapping loggers to targets
-            // 该方法所设置节点的属性必须关联其它的兄弟节点或者属性为布尔值
-            config.AddRule(NLog.LogLevel.Info, NLog.LogLevel.Fatal, logconsole);
-            config.AddRule(NLog.LogLevel.Debug, NLog.LogLevel.Fatal, logfile);
-
-            // Apply config           
-            NLog.LogManager.Configuration = config;
-        }
         private void PLCReadAction(CancellationToken token)
         {
-            while (true)
-            {
-                if (token.IsCancellationRequested)
-                {
-                    return;
-                }
-                try
-                {
-                    //var coils = pLC.ReadXCoils(0, 48);
-                    //for (int i = 0; i < 48; i++)
-                    //{
-                    //    pLC.XCoils_X0[i] = coils[i];
-                    //}
-                    //coils = pLC.ReadYCoils(16, 30);
-                    //for (int i = 0; i < 30; i++)
-                    //{
-                    //    pLC.YCoils_Y20[i] = coils[i];
-                    //    var a = pLC.YCoils_Y20[i];
-                    //}
-                    //Console.WriteLine(Properties.Settings.Default.PLC_IP);
-                }
-                catch (Exception ex)
-                {
-                    if (ex is SocketException || ex is InvalidOperationException)
-                    {
-                        pLC.Close();
-                        bool r;
-                        PLCState = r = pLC.Connect(Properties.Settings.Default.PLC);
-                        logger.Error($"PLC重连{(r ? "成功" : "失败")}");
-                    }
-                }
 
-                Thread.Sleep(200);
-            }
         }
         private void ReadWaring(string path)
         {
@@ -338,7 +270,7 @@ namespace MonitorApp.ViewModels
                     {
                         报警地址 = ushort.Parse(line.Split('\t')[0].Replace("M", "")),
                         报警内容 = line.Split('\t')[1],
-                    }); 
+                    });
                     上升沿List.Add(new 上升沿());
                 }
                 ushort 起始地址 = 300;
@@ -355,13 +287,13 @@ namespace MonitorApp.ViewModels
                             addMessage($"有过{报警信息.报警地址}:{报警信息.报警内容}报警");
                         }
                     }
-                    
+
                 }
             }
         }
         #endregion
         #region 数据库功能函数
-        private void MySQL_ConnectionMsg()//数据库连接的构建
+        public void MySQL_ConnectionMsg()//数据库连接的构建
         {
             //构建连接字符串
             builder.Server = Settings.Default.MySQL_Server;
@@ -370,39 +302,7 @@ namespace MonitorApp.ViewModels
             builder.Password = Settings.Default.MySQL_Password;
             builder.Database = Settings.Default.MySQL_Database;
         }
-        private void Insert_pc_data_alarm(string 报警代码,string 报警描述, bool value)//设备报警描述
-        {
-            var connection = new MySqlConnection(builder.ConnectionString);
-            try
-            {
-                // 打开连接
-                connection.Open();
-                // 执行插入数据的 SQL 语句
-                string sql = "INSERT INTO pc_data_alarm (ALARMCODE, ALARMDES, ALARMVALUE, CREATETIME) " +
-                             "VALUES (@AlarmCode, @AlarmDes, @AlarmValue, @CreateTime)";
-                MySqlCommand command = new MySqlCommand(sql, connection);
-                // 添加参数
-                command.Parameters.AddWithValue("@AlarmCode", 报警代码);
-                command.Parameters.AddWithValue("@AlarmDes", 报警描述);
-                command.Parameters.AddWithValue("@AlarmValue", value);//报警1， 复位0
-                command.Parameters.AddWithValue("@CreateTime", DateTime.Now);
-
-                command.ExecuteNonQuery();
-
-                addMessage("设备报警描述数据添加成功！");
-            }
-            catch (Exception ex)
-            {
-                addMessage("设备报警描述异常" + ex);
-            }
-            finally
-            {
-                // 关闭连接
-                if (connection.State == System.Data.ConnectionState.Open)
-                    connection.Close();
-            }
-        }
-        private void Insert_pc_data_craft(string 批次号,string 版号,string 项目代码,string 项目描述,string 单位,string 采集值,string isok)//工艺质量
+        private void Insert_pc_data_craft(string 批次号, string 版号, string 项目代码, string 项目描述, string 单位, string 采集值, string isok)//工艺质量
         {
             var connection = new MySqlConnection(builder.ConnectionString);
             try
@@ -440,7 +340,7 @@ namespace MonitorApp.ViewModels
                     connection.Close();
             }
         }
-        private void Insert_pc_data_emp(string 员工号,string 员工姓名,string 操作代码,string 操作名称)//员工上下机记录上传
+        private void Insert_pc_data_emp(string 员工号, string 员工姓名, string 操作代码, string 操作名称)//员工上下机记录上传
         {
             var connection = new MySqlConnection(builder.ConnectionString);
             try
@@ -475,7 +375,7 @@ namespace MonitorApp.ViewModels
                     connection.Close();
             }
         }
-        private void Insert_pc_signal_status(string 信号代码,string 信号描述,string 信号值)//设备实时信号
+        private void Insert_pc_signal_status(string 信号代码, string 信号描述, string 信号值)//设备实时信号
         {
             // 创建 MySqlConnection 对象
             MySqlConnection connection = new MySqlConnection(builder.ConnectionString);
